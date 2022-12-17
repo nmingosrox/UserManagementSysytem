7dfd3455555cc176b2da81b5cd80eb7b3fde3725
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2022-12-17 09:00:19 +0200

    Update User.py

--- User.py
+++ User.py
@@ -1,2 +1,8 @@
-class Person():
-  
+class User():
+  def __init__(self, username, password):
+    self.name = name
+    self.password = password
+   
+  def login(self):
+    if self.password == stored_password:
+      open(f'{user}_log.txt')
